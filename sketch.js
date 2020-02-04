c3a796266e4620b629fe35c1140ea4cac0825dc4
Debb878 <garcd878@gmail.com>
2020-02-04 11:33:44 -0800

    added shape

--- sketch.js
+++ sketch.js
@@ -2,19 +2,27 @@
 
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(720, 400,WEBGL);
   background(0);
-  noStroke();
-  noLoop();
+   
 }
 
 function draw() {
-
+ 
+  translate(40,60);  
+  drawMoon(20);
+//drawMoon(frames%30, 20, 20);
+//drawMoon(frames%15, 60, 70);
 
 }
 
 
-function drawMoon(){
+function drawMoon(phase, posx, posy,radius){
+
+  fill(34,55,123);
+  translate(posx,posy);
+  sphere(radius)
+ // ellipse(posx, posy, phase, phase);
 
  	
 	
